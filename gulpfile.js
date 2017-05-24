aa1e9a76c47f01f4434d5adaba559cfcd7a02bc1
Masayuki Komatsu <komatsu-m@gnavi.co.jp>
2017-05-24 17:09:54 +0900

    add gulp plugin "gulp-concat-css"

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCss = require('gulp-clean-css');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('sass' , function () {
   return gulp.src('./sass/*.scss')
-	 .pipe(sass())
-	 .pipe(cleanCss())
-	 .pipe(gulp.dest('./dist/css'));
+    .pipe(concatCss('vue-transition.css'))
+    .pipe(sass())
+    .pipe(cleanCss())
+    .pipe(gulp.dest('./dist/css'));
 });
 
 
